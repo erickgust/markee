a59c7d82524da6f36bc686baa968eb733a5a3f82
Erick Gustavo <erick.stazs@gmail.com>
2022-07-16 22:39:05 -0300

    Clean up CRA

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import reportWebVitals from './reportWebVitals'
+import { createRoot } from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-)
-root.render(
+const root = document.querySelector('[data-js="root"]')!
+
+createRoot(root).render(
   <StrictMode>
-    <App />
+    <h1>App</h1>
   </StrictMode>,
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
