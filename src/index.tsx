3b946fd46c9773bd021893faf9fbdcbdb2cf54d9
Erick Gustavo <erick.stazs@gmail.com>
2022-07-18 20:30:55 -0300

    Add root

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Root } from 'root'
 
 const root = document.querySelector('[data-js="root"]')!
 
 createRoot(root).render(
   <StrictMode>
-    <h1>App</h1>
+    <Root />
   </StrictMode>,
 )
