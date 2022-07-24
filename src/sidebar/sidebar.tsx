81477578386113a04547d966bfee34ace6598439
Erick Gustavo <erick.stazs@gmail.com>
2022-07-24 19:30:25 -0300

    Add Markee logo

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components/macro'
 import { Files } from './files'
+import markeeLogo from './markee-logo.svg'
 
 const SidebarStyled = styled.aside`
+  text-align: center;
   max-width: 33rem;
   width: 100%;
   background-color: ${({ theme }) => theme.colors.black};
@@ -11,6 +13,11 @@ const SidebarStyled = styled.aside`
 function Sidebar () {
   return (
     <SidebarStyled>
+      <h1>
+        <a href='/'>
+          <img src={markeeLogo} alt='Markee.' />
+        </a>
+      </h1>
       <Files />
     </SidebarStyled>
   )
