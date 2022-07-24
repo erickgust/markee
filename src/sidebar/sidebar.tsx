6d09039b4548b9ff006278a670a68ff3de651ab6
Erick Gustavo <erick.stazs@gmail.com>
2022-07-24 20:45:37 -0300

    Add title for files

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,23 +1,52 @@
-import styled from 'styled-components/macro'
+import styled, { css } from 'styled-components/macro'
 import { Files } from './files'
 import markeeLogo from './markee-logo.svg'
 
 const SidebarStyled = styled.aside`
-  text-align: center;
   max-width: 33rem;
   width: 100%;
   background-color: ${({ theme }) => theme.colors.black};
   padding: 3.2rem;
 `
 
+const H2 = styled.h2`${({ theme }) => css`
+  color: ${theme.colors.white};
+  position: relative;
+  padding-left: 2rem;
+  font-size: 1.6rem;
+
+  &::before {
+    content: '';
+    position: absolute;
+    border-bottom: 2px solid ${theme.colors.primary};
+    border-radius: 5px;
+    width: 100%;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  > span {
+    background-color: ${theme.colors.black};
+    position: relative;
+    padding: 0 1rem;
+  }
+`}`
+
 function Sidebar () {
   return (
     <SidebarStyled>
-      <h1>
+      <h1 style={{ textAlign: 'center' }}>
         <a href='/'>
           <img src={markeeLogo} alt='Markee.' />
         </a>
       </h1>
+
+      <H2>
+        <span>
+          Arquivos
+        </span>
+      </H2>
       <Files />
     </SidebarStyled>
   )
