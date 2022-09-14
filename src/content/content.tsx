81af9c0485d8066a66fb3ce280125e6706e60bab
Erick Gustavo <erick.stazs@gmail.com>
2022-09-14 20:38:49 -0300

    Add marked, highlight.js and dompurify

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,13 +1,43 @@
+import { ChangeEvent, useState } from 'react'
 import * as S from './content-styles'
+import { marked } from 'marked'
+import { sanitize } from 'dompurify'
+
+import 'highlight.js/styles/github.css'
+
+import('highlight.js').then(({ default: hljs }) => {
+  marked.setOptions({
+    highlight: (code, language) => {
+      if (language && hljs.getLanguage(language)) {
+        return hljs.highlight(code, { language }).value
+      }
+
+      return hljs.highlightAuto(code).value
+    },
+  })
+})
 
 function Content () {
+  const [content, setContent] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.currentTarget.value)
+  }
+
   return (
     <S.Content>
-      <S.Textarea name='editor' id='editor' placeholder='Digite aqui seu markdown' />
-      <S.Div>
-        <h1>Bootcamp Brainn Co.</h1>
-        <p>Lorem ipsum dolor sit amet simet</p>
-      </S.Div>
+      <S.Textarea
+        name='editor'
+        id='editor'
+        placeholder='Digite aqui seu markdown'
+        value={content}
+        onChange={handleChange}
+      />
+      <S.Div
+        dangerouslySetInnerHTML={{
+          __html: sanitize(marked.parse(content)),
+        }}
+      />
     </S.Content>
   )
 }
