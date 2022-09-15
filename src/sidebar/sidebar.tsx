6a9bfe1eb1bd8b74c042c77162fdac4951397faa
Erick Gustavo <erick.stazs@gmail.com>
2022-09-15 15:25:47 -0300

    Add files state and handleNewFile function

--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,8 +1,46 @@
+import { useState } from 'react'
+import { v4 } from 'uuid'
 import { Files } from './files'
+import { File } from './files/types'
 import markeeLogo from './markee-logo.svg'
 import * as S from './sidebar-styles'
 
 function Sidebar () {
+  const [files, setFiles] = useState<File[]>([{
+    id: v4(),
+    name: 'README.md',
+    content: 'Readme',
+    active: false,
+    status: 'saved',
+  },
+  {
+    id: v4(),
+    name: 'CONTRIBUTING.md',
+    content: 'Contributing',
+    active: false,
+    status: 'saved',
+  },
+  {
+    id: v4(),
+    name: 'LICENSE.md',
+    content: 'License',
+    active: false,
+    status: 'saved',
+  }])
+
+  const handleNewFile = () => {
+    setFiles(files => files
+      .map(file => ({ ...file, active: false }))
+      .concat({
+        id: v4(),
+        name: 'Sem título',
+        content: '',
+        active: true,
+        status: 'saved',
+      }),
+    )
+  }
+
   return (
     <S.Aside>
       <S.H1>
@@ -15,9 +53,9 @@ function Sidebar () {
         <span>Arquivos</span>
       </S.H2>
 
-      <S.NewFileButton>Adicionar arquivo</S.NewFileButton>
+      <S.NewFileButton onClick={handleNewFile}>Adicionar arquivo</S.NewFileButton>
 
-      <Files />
+      <Files files={files} />
     </S.Aside>
   )
 }
