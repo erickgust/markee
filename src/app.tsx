0ad7a3f6ac323fd88da969240d51d9de7e46960a
Erick Gustavo <erick.stazs@gmail.com>
2022-09-17 20:38:05 -0300

    Updates the active file with the current title and content

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
-import { useRef } from 'react'
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react'
 import { Content } from 'content'
 import { Header } from 'header'
 import { Sidebar } from 'sidebar'
 import styled from 'styled-components/macro'
+import { v4 } from 'uuid'
+import { File } from 'sidebar/files/types'
 
 const Main = styled.main`
   display: flex;
@@ -18,14 +20,81 @@ const Section = styled.section`
 `
 
 function App () {
+  const initialTitle = 'Sem título'
   const inputRef = useRef<HTMLInputElement>(null)
+
+  const [title, setTitle] = useState(initialTitle)
+  const [content, setContent] = useState('')
+  const [editing, setEditing] = useState(false)
+  const [files, setFiles] = useState<File[]>([])
+
+  const handleNewFile = () => {
+    inputRef.current?.focus()
+
+    setFiles(files => files
+      .map(file => ({ ...file, active: false }))
+      .concat({
+        id: v4(),
+        name: 'Sem título',
+        content: '',
+        active: true,
+        status: 'saved',
+      }),
+    )
+  }
+
+  const handleChangeTitle = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value)
+    setEditing(true)
+  }, [])
+
+  const handleChangeContent = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.currentTarget.value)
+    setEditing(true)
+  }, [])
+
+  const handleFileSave = useCallback((title: string, content: string) => {
+    setFiles(files => (
+      files.map(file => {
+        if (file.active) {
+          return {
+            ...file,
+            name: title || initialTitle,
+            content,
+          }
+        }
+
+        return file
+      })
+    ))
+  }, [])
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>
+
+    if (editing) {
+      timer = setTimeout(() => {
+        handleFileSave(title, content)
+        setEditing(false)
+      }, 300)
+    }
+
+    return () => clearTimeout(timer)
+  }, [title, content, editing, handleFileSave])
 
   return (
     <Main>
-      <Sidebar inputRef={inputRef} />
+      <Sidebar files={files} onNewFile={handleNewFile} />
       <Section>
-        <Header inputRef={inputRef} />
-        <Content />
+        <Header
+          inputRef={inputRef}
+          onChangeTitle={handleChangeTitle}
+          title={title}
+        />
+        <Content
+          onChangeContent={handleChangeContent}
+          content={content}
+        />
       </Section>
     </Main>
   )
--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,52 +1,14 @@
-import { RefObject, useState } from 'react'
-import { v4 } from 'uuid'
 import { Files } from './files'
 import { File } from './files/types'
 import markeeLogo from './markee-logo.svg'
 import * as S from './sidebar-styles'
 
 type SidebarProps = {
-  inputRef: RefObject<HTMLInputElement>
+  onNewFile: () => void
+  files: File[]
 }
 
-function Sidebar ({ inputRef }: SidebarProps) {
-  const [files, setFiles] = useState<File[]>([{
-    id: v4(),
-    name: 'README.md',
-    content: 'Readme',
-    active: false,
-    status: 'saved',
-  },
-  {
-    id: v4(),
-    name: 'CONTRIBUTING.md',
-    content: 'Contributing',
-    active: false,
-    status: 'saved',
-  },
-  {
-    id: v4(),
-    name: 'LICENSE.md',
-    content: 'License',
-    active: false,
-    status: 'saved',
-  }])
-
-  const handleNewFile = () => {
-    inputRef.current?.focus()
-
-    setFiles(files => files
-      .map(file => ({ ...file, active: false }))
-      .concat({
-        id: v4(),
-        name: 'Sem título',
-        content: '',
-        active: true,
-        status: 'saved',
-      }),
-    )
-  }
-
+function Sidebar ({ files, onNewFile }: SidebarProps) {
   return (
     <S.Aside>
       <S.H1>
@@ -59,7 +21,7 @@ function Sidebar ({ inputRef }: SidebarProps) {
         <span>Arquivos</span>
       </S.H2>
 
-      <S.NewFileButton onClick={handleNewFile}>Adicionar arquivo</S.NewFileButton>
+      <S.NewFileButton onClick={onNewFile}>Adicionar arquivo</S.NewFileButton>
 
       <Files files={files} />
     </S.Aside>
